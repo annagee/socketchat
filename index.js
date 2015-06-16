58fa7f2ec3a0ac390a3735c60fc3b4062cc2e0d5
Anna Gaughan <agaughan@gmail.com>
2015-06-16 18:22:27 +0100

    chat template on page

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 var express = require('express'); // Use the express library. 
-var app = express(); // Create our app. 
-
+var app = express(); // Create our app./ var server = require('http').createServer(app);
 var http = require('http');
 server = http.createServer(app); // Create an HTTP server.
-server.listen(process.env.PORT || 4000); // Listen on the default port, or on 4000 
+server.listen(process.env.PORT || 4000); // Listen on the default port, or on 4000
+
+
+
+ app.set('views', './views');
+ app.set('view engine', 'ejs');
+ app.use(express.static(__dirname + '/public'));
+
+ app.get('/', function(req, res){
+  res.render('index')
+ })
+
+
+
